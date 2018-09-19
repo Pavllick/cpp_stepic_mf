5a4ec753b963867fd19b085d76c49244a6a9e369
pavlick <pavllick@gmail.com>
2018-09-19 12:10:46 -0400

    final web server

---
+++ 9_1/CMakeFiles/final.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/pavlick/cpr/mt_cource/9_1/final.cpp" "/Users/pavlick/cpr/mt_cource/9_1/CMakeFiles/final.dir/final.cpp.o"
+  "/Users/pavlick/cpr/mt_cource/9_1/src/sock_con.cpp" "/Users/pavlick/cpr/mt_cource/9_1/CMakeFiles/final.dir/src/sock_con.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "inc"
+  "src"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
